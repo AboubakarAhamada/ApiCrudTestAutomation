47f97c27c437029ab8a0bbe7eccb6024e6990474
AboubakarAhamada <aboumarshal@hotmail.fr>
2026-02-15 16:22:18 +0100

    Ajout de la classe CucumberTestSuite

---
+++ src/test/java/runners/CucumberTestSuite.java
@@ -0,0 +1,16 @@
+package runners;
+
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, json:target/cucumber.json, html:target/cucumber.html")
+public class CucumberTestSuite {
+}
